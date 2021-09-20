7f59dc3ae50b2c429947a8ac16309548bf7b382d
Albert Minnie <albert.minnie@gmail.com>
2021-09-20 14:44:39 +0200

    add api call for searching for artists using node-fetch

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,15 +4,37 @@
  */
 
 import * as express from 'express';
+import fetch from 'node-fetch';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get('/healthcheck', (req, res) => {
+  res.status(200).json({ status: 'API online' });
+});
+
+app.get('/artists', async (req, res) => {
+  console.log(req.query.q);
+  const response = await fetch(
+    `https://api.deezer.com/search/artist/?q=${req.query.q}`
+  );
+  const body = await response.json();
+
+  let result = [];
+  for (let i = 0; i < body.data.length; i++) {
+    const data = body.data[i];
+    result.push({
+      id: data.id,
+      name: data.name,
+      img: data.picture_medium,
+      fans: data.nb_fan,
+    });
+  }
+
+  res.status(200).json(result);
 });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
