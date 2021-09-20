fac11be941214ce59cead33e714bf05ff8206f44
Albert Minnie <albert.minnie@gmail.com>
2021-09-20 15:57:00 +0200

    remove unused import

--- apps/wyzetalk-assessment/src/app/components/artist/artist.component.ts
+++ apps/wyzetalk-assessment/src/app/components/artist/artist.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'wyzetalk-artist',
