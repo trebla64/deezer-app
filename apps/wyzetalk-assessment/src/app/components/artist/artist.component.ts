93cdd6a7014631324b8b1cfb84d5114a5f4785bd
Albert Minnie <albert.minnie@gmail.com>
2021-09-22 10:54:34 +0200

    add loading of albums on artist detail screen

--- apps/wyzetalk-assessment/src/app/components/artist/artist.component.ts
+++ apps/wyzetalk-assessment/src/app/components/artist/artist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Album, DeezerService } from '../../services/deezer.service';
 
 @Component({
   selector: 'wyzetalk-artist',
@@ -8,8 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArtistComponent implements OnInit {
   private selectedId: number;
+  public albums: Album[] = [];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private deezerService: DeezerService
+  ) {
     this.selectedId = 0;
   }
 
@@ -17,6 +22,16 @@ export class ArtistComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.selectedId = Number(params.get('id'));
       console.log('selectedId: ', this.selectedId);
+      this.refreshAlbums();
     });
+  }
+
+  async refreshAlbums() {
+    try {
+      this.albums = await this.deezerService.getAlbums(this.selectedId);
+      console.log('albums: ', this.albums);
+    } catch (error) {
+      console.log('Error: ', error);
+    }
   }
 }
--- apps/wyzetalk-assessment/src/app/services/deezer.service.ts
+++ apps/wyzetalk-assessment/src/app/services/deezer.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API_URL = 'http://localhost:3333/';
+const API_URL = 'http://localhost:3333';
 
 export interface Artist {
   id: number;
   name: string;
   img: string;
   fans: number;
+}
+
+export interface Album {
+  id: number;
+  name: string;
+  img: string;
+  fans: number;
+  release_date: string;
 }
 
 @Injectable({
@@ -17,6 +25,14 @@ export class DeezerService {
   constructor(private http: HttpClient) {}
 
   getArtists(artist: string): Promise<Artist[]> {
-    return this.http.get<Artist[]>(`${API_URL}artists?q=${artist}`).toPromise();
+    return this.http
+      .get<Artist[]>(`${API_URL}/artists?q=${artist}`)
+      .toPromise();
+  }
+
+  getAlbums(artist_id: number): Promise<Album[]> {
+    return this.http
+      .get<Album[]>(`${API_URL}/artist/${artist_id}/albums`)
+      .toPromise();
   }
 }
