2611d713a11dab80072bf29275b004008b4cf811
Albert Minnie <albert.minnie@gmail.com>
2021-09-27 12:02:09 +0200

    add unit test for pipe

--- apps/wyzetalk-assessment/src/app/pipes/thousands-formatter.pipe.spec.ts
+++ apps/wyzetalk-assessment/src/app/pipes/thousands-formatter.pipe.spec.ts
@@ -5,4 +5,10 @@ describe('ThousandsFormatterPipe', () => {
     const pipe = new ThousandsFormatterPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('#transform gives the correct output', () => {
+    const pipe = new ThousandsFormatterPipe();
+    const transformed = pipe.transform(12876);
+    expect(transformed).toBe('12.88k');
+  });
 });
