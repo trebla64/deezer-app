ca8ff97982896b33feda01bef001fbdc3013b117
Albert Minnie <albert.minnie@gmail.com>
2021-09-27 11:27:06 +0200

    add unit tests for getting data from deezer api

--- apps/wyzetalk-assessment/src/app/services/deezer.service.spec.ts
+++ apps/wyzetalk-assessment/src/app/services/deezer.service.spec.ts
@@ -1,18 +1,86 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DeezerService } from './deezer.service';
+import { Album, Artist, DeezerService } from './deezer.service';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('DeezerService', () => {
   let service: DeezerService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(DeezerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getArtists should make a `GET` request', () => {
+    // Setup
+    const artists: Artist[] = [
+      {
+        id: 1,
+        name: 'Test Artist #1',
+        img: 'test1.jpg',
+        fans: 1000,
+      },
+      {
+        id: 2,
+        name: 'Test Artist #2',
+        img: 'test2.jpg',
+        fans: 2000,
+      },
+    ];
+
+    // Actual test
+    service.getArtists('test').then((resp) => {
+      expect(resp).toEqual(artists);
+    });
+
+    const req = httpTestingController.expectOne(
+      service.API_URL + '/artists?q=test'
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(artists);
+  });
+
+  it('#getAlbums should make a `GET` request', () => {
+    // Setup
+    const albums: Album[] = [
+      {
+        id: 1,
+        name: 'Test Album #1',
+        img: 'test1.jpg',
+        fans: 1000,
+        release_date: '2021-09-27',
+      },
+      {
+        id: 2,
+        name: 'Test Artist #2',
+        img: 'test2.jpg',
+        fans: 2000,
+        release_date: '2020-09-27',
+      },
+    ];
+
+    // Actual test
+    const artistId = 13;
+    service.getAlbums(artistId).then((resp) => {
+      expect(resp).toEqual(albums);
+    });
+
+    const req = httpTestingController.expectOne(
+      `${service.API_URL}/artist/${artistId}/albums`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(albums);
   });
 });
--- apps/wyzetalk-assessment/src/app/services/deezer.service.ts
+++ apps/wyzetalk-assessment/src/app/services/deezer.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 // TODO: This should be moved to environment file
-const API_URL = 'http://localhost:3333';
+// const API_URL = 'http://localhost:3333';
 
 export interface Artist {
   id: number;
@@ -29,23 +29,25 @@ export interface Track {
   providedIn: 'root',
 })
 export class DeezerService {
+  API_URL = 'http://localhost:3333';
+
   constructor(private http: HttpClient) {}
 
   getArtists(artist: string): Promise<Artist[]> {
     return this.http
-      .get<Artist[]>(`${API_URL}/artists?q=${artist}`)
+      .get<Artist[]>(`${this.API_URL}/artists?q=${artist}`)
       .toPromise();
   }
 
   getAlbums(artist_id: number): Promise<Album[]> {
     return this.http
-      .get<Album[]>(`${API_URL}/artist/${artist_id}/albums`)
+      .get<Album[]>(`${this.API_URL}/artist/${artist_id}/albums`)
       .toPromise();
   }
 
   getTop5(artist_id: number): Promise<Track[]> {
     return this.http
-      .get<Track[]>(`${API_URL}/artist/${artist_id}/top5`)
+      .get<Track[]>(`${this.API_URL}/artist/${artist_id}/top5`)
       .toPromise();
   }
 }
